f2305d1b1c35ff74502036b81ad29b3246b62a14
Steven Smith <sds2870@yahoo.com>
2022-01-24 00:34:22 -0600

    Occupations set. On to the States

---
+++ code.js
@@ -0,0 +1,249 @@
+// Occupation Object:
+// {
+//     "occupations" = [
+//       "Head of Shrubbery",
+//       "Interim Substitute Teacher",
+//       "Water Softener",
+//       "Listener of the House",
+//       "Really Good Dancer",
+//       "Gainfully Unemployed",
+//       "Alexa Impersonator",
+//       "Chard Farmer",
+//       "Chief Frolicker (Jolly)",
+//       "Entry-level Seat Recliner",
+//       "CEO (Summer Internship)",
+//       "Widget Fabricator",
+//       "Underwater Basket Weaver"
+//     ]
+// }
+
+// "states": [
+//     {
+//       "name": "Alabama",
+//       "abbreviation": "AL"
+//     },
+//     {
+//       "name": "Alaska",
+//       "abbreviation": "AK"
+//     },
+//     {
+//       "name": "Arizona",
+//       "abbreviation": "AZ"
+//     },
+//     {
+//       "name": "Arkansas",
+//       "abbreviation": "AR"
+//     },
+//     {
+//       "name": "California",
+//       "abbreviation": "CA"
+//     },
+//     {
+//       "name": "Colorado",
+//       "abbreviation": "CO"
+//     },
+//     {
+//       "name": "Connecticut",
+//       "abbreviation": "CT"
+//     },
+//     {
+//       "name": "Delaware",
+//       "abbreviation": "DE"
+//     },
+//     {
+//       "name": "District Of Columbia",
+//       "abbreviation": "DC"
+//     },
+//     {
+//       "name": "Florida",
+//       "abbreviation": "FL"
+//     },
+//     {
+//       "name": "Georgia",
+//       "abbreviation": "GA"
+//     },
+//     {
+//       "name": "Hawaii",
+//       "abbreviation": "HI"
+//     },
+//     {
+//       "name": "Idaho",
+//       "abbreviation": "ID"
+//     },
+//     {
+//       "name": "Illinois",
+//       "abbreviation": "IL"
+//     },
+//     {
+//       "name": "Indiana",
+//       "abbreviation": "IN"
+//     },
+//     {
+//       "name": "Iowa",
+//       "abbreviation": "IA"
+//     },
+//     {
+//       "name": "Kansas",
+//       "abbreviation": "KS"
+//     },
+//     {
+//       "name": "Kentucky",
+//       "abbreviation": "KY"
+//     },
+//     {
+//       "name": "Louisiana",
+//       "abbreviation": "LA"
+//     },
+//     {
+//       "name": "Maine",
+//       "abbreviation": "ME"
+//     },
+//     {
+//       "name": "Maryland",
+//       "abbreviation": "MD"
+//     },
+//     {
+//       "name": "Massachusetts",
+//       "abbreviation": "MA"
+//     },
+//     {
+//       "name": "Michigan",
+//       "abbreviation": "MI"
+//     },
+//     {
+//       "name": "Minnesota",
+//       "abbreviation": "MN"
+//     },
+//     {
+//       "name": "Mississippi",
+//       "abbreviation": "MS"
+//     },
+//     {
+//       "name": "Missouri",
+//       "abbreviation": "MO"
+//     },
+//     {
+//       "name": "Montana",
+//       "abbreviation": "MT"
+//     },
+//     {
+//       "name": "Nebraska",
+//       "abbreviation": "NE"
+//     },
+//     {
+//       "name": "Nevada",
+//       "abbreviation": "NV"
+//     },
+//     {
+//       "name": "New Hampshire",
+//       "abbreviation": "NH"
+//     },
+//     {
+//       "name": "New Jersey",
+//       "abbreviation": "NJ"
+//     },
+//     {
+//       "name": "New Mexico",
+//       "abbreviation": "NM"
+//     },
+//     {
+//       "name": "New York",
+//       "abbreviation": "NY"
+//     },
+//     {
+//       "name": "North Carolina",
+//       "abbreviation": "NC"
+//     },
+//     {
+//       "name": "North Dakota",
+//       "abbreviation": "ND"
+//     },
+//     {
+//       "name": "Ohio",
+//       "abbreviation": "OH"
+//     },
+//     {
+//       "name": "Oklahoma",
+//       "abbreviation": "OK"
+//     },
+//     {
+//       "name": "Oregon",
+//       "abbreviation": "OR"
+//     },
+//     {
+//       "name": "Pennsylvania",
+//       "abbreviation": "PA"
+//     },
+//     {
+//       "name": "Rhode Island",
+//       "abbreviation": "RI"
+//     },
+//     {
+//       "name": "South Carolina",
+//       "abbreviation": "SC"
+//     },
+//     {
+//       "name": "South Dakota",
+//       "abbreviation": "SD"
+//     },
+//     {
+//       "name": "Tennessee",
+//       "abbreviation": "TN"
+//     },
+//     {
+//       "name": "Texas",
+//       "abbreviation": "TX"
+//     },
+//     {
+//       "name": "Utah",
+//       "abbreviation": "UT"
+//     },
+//     {
+//       "name": "Vermont",
+//       "abbreviation": "VT"
+//     },
+//     {
+//       "name": "Virginia",
+//       "abbreviation": "VA"
+//     },
+//     {
+//       "name": "Washington",
+//       "abbreviation": "WA"
+//     },
+//     {
+//       "name": "West Virginia",
+//       "abbreviation": "WV"
+//     },
+//     {
+//       "name": "Wisconsin",
+//       "abbreviation": "WI"
+//     },
+//     {
+//       "name": "Wyoming",
+//       "abbreviation": "WY"
+//     }
+//   ]
+
+
+// I need to create a variable to house my url for the fetch.
+
+
+// I now must perform a fetch to ensure that everything is as it should be
+
+    
+    // const goFetch = "https://frontend-take-home.fetchrewards.com/form"
+    // fetch(goFetch, {
+    //     method:"POST",
+    //     headers: {
+    //         "Content-type": "application/json"
+    //     },
+    //     body: JSON.stringify(body),
+    // })
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+
+    
+
+
+// Now, let's create a function for our fetch.
+
